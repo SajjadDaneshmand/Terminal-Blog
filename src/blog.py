8df8603a6924036a499aa348b3c80876febad49e
sajjad <Mr.SajjadDaneshmand@gmail.com>
2021-06-27 12:37:33 +0430

    Create project structure

---
+++ src/blog.py
@@ -0,0 +1,27 @@
+# standard
+import sqlite3
+import os
+
+################### CREATE TABLE AND DATABASE ###################
+
+def create_database(path):
+    pass
+
+def create_table(name):
+    pass
+
+#################### INSERT DATA TO DATABASE ####################
+
+def insert_post(post):
+    pass
+
+def insert_description(description):
+    pass
+
+#################### READ DATA FROM DATABASE ####################
+
+def read_all_post():
+    pass
+
+def read_all_description():
+    pass
